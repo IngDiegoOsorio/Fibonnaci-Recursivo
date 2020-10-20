5302db428f727d0f75a0e420ee25d76b6e995e95
Diego Osorio <juan.osorio1599@alumnos.udg.mx>
2020-10-20 15:46:41 -0500

    Se crea funcion imprimir_fibonacci que imprime la serie de fibonacci recibiendo como parametro el numero de iteraciones

--- main.go
+++ main.go
@@ -5,11 +5,18 @@ func fibonacci(limite uint) uint{
 	if limite == 0 || limite == 1 {
 		return limite
 	}
-
 	return fibonacci(limite-2) + fibonacci(limite-1)
 }
 
+func imprimir_fibonacci(iteraciones uint){
+	var n uint
+	for n = 0 ; n < iteraciones; n++ {
+		fmt.Printf("| %d ", fibonacci(n))
+	  }
+	  fmt.Printf("|\n")
+}
 func main(){
-	var x = fibonacci(10)
-	fmt.Println(x)
+	
+	imprimir_fibonacci(15)
+
 }
